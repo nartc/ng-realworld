832cdad71ed97e989e35dcb602ffbc4057d984c7
Chau Tran <nartc7789@gmail.com>
2024-01-13 20:17:02 -0600

    prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    
-  `,
-  styles: [],
+	selector: 'app-root',
+	standalone: true,
+	imports: [],
+	template: ` <h1>Welcome to {{ title }}!</h1> `,
+	styles: [],
 })
 export class AppComponent {
-  title = 'ng-realworld';
+	title = 'ng-realworld';
 }
